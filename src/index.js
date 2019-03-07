a6bb8345dcecc24530db162174dec76fd6cb78c1
lyn <buaaxigua@gmail.com>
2019-03-08 01:57:40 +0800

    add: dynamic import

--- src/index.js
+++ src/index.js
@@ -6,19 +6,31 @@ if (process.env.NODE_ENV !== 'production') {
   console.log('Looks like we are in development mode!');
 }
 
-function component() {
-  let element = document.createElement('pre');
+// function component() {
+//   let element = document.createElement('pre');
 
-  // Lodash, currently included via a script, is required for this line to work
-  // element.innerHTML = _.join(['Hello', 'webpack'], 'sadasdasd ');
-  element.innerHTML = [
-    'Hello webpack!',
-    '5 cubed is equal to ' + cube(5)
-  ].join('\n\n');
+//   // Lodash, currently included via a script, is required for this line to work
+//   // element.innerHTML = _.join(['Hello', 'webpack'], 'sadasdasd ');
+//   element.innerHTML = [
+//     'Hello webpack!',
+//     '5 cubed is equal to ' + cube(5)
+//   ].join('\n\n');
 
-  element.classList.add('hello');
+//   element.classList.add('hello');
 
-  return element;
+//   return element;
+// }
+
+// document.body.appendChild(component());
+
+function getComponent() {
+  return import(/* webpackChunkName: "lodash" */ 'lodash').then(({ default: _ }) => {
+    var element = document.createElement('div');
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    return element;
+  }).catch(error => 'An error occurred while loading the component');
 }
 
-document.body.appendChild(component());
+getComponent().then(component => {
+  document.body.appendChild(component);
+})
--- webpack.common.js
+++ webpack.common.js
@@ -9,7 +9,7 @@ module.exports = {
     another: './src/another-module.js'
   },
   plugins: [
-    // new CleanWebpackPlugin(),
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       title: 'Production'
     }),
@@ -22,6 +22,7 @@ module.exports = {
   },
   output: {
     filename: '[name].bundle.js',
+    chunkFilename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
   }
 };
